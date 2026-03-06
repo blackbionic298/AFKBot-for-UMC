becc72cebe8b40a566ee2fa97212be61e50523e0
blackbionic298 <blackbionic298@gmail.com>
2026-03-06 16:17:44 +0100

    index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const CONFIG = {
   checkTimeoutInterval: 300000      // 延长到 5 分钟
 };
 
-const BOT_USERNAME = 'accountcontrol';
+const BOT_USERNAME = 'HostByAI';
 const AUTHME_PASSWORD = process.env.AUTHME_PASSWORD || 'deutschland';
 
 let bot;
